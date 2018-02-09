210a2a3999cfe1f8bc89737adbca5e8364a6272e
rikumi <riku.mi@outlook.com>
2018-02-09 08:41:32 +0800

    更新首页

--- utils/noie.js
+++ utils/noie.js
@@ -1,3 +1,3 @@
 if (!!window.ActiveXObject || "ActiveXObject" in window) {
-  document.body.innerHTML = '<p style="position:absolute;top:50%;left:0;right:0;margin-top:-1em;line-height:2em;font-family:sans-serif;color:#bbb;text-align:center">此站点不支持 IE，请使用 Chrome 或 Safari 浏览器</p>'
+  document.body.innerHTML = '<p style="position:absolute;top:50%;left:0;right:0;margin-top:-1em;line-height:2em;font-family:sans-serif;color:#bbb;text-align:center">此站点不支持 IE 内核，请使用 Chrome 或 Safari 浏览器<br/>如果你使用双核浏览器，请切换到极速模式</p>'
 }
