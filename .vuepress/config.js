0c652872bff2c41b380dfc57548b043354fe3e63
rikumiyu <rikumiyu@tencent.com>
2019-11-06 01:41:47 +0800

    deploy

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -6,6 +6,9 @@ module.exports = {
   },
   title: '[ RKM ]',
   description: 'アクアテラリウム',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.png', type: 'image/x-icon' }]
+  ],
   theme: 'journal',
   themeConfig: {
     nav: [
