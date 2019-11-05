0a4419170537a87f52a95fe97717b2031320759a
rikumiyu <rikumiyu@tencent.com>
2019-11-06 01:48:32 +0800

    deploy

--- .vuepress/deploy.sh
+++ .vuepress/deploy.sh
@@ -8,6 +8,11 @@ git add -A
 git commit -m 'deploy'
 
 git push -f https://github.com/rikumi/rikumi.github.io.git master
+touch .trigger-build
+git add .
+git commit -m 'deploy'
+git push https://github.com/rikumi/rikumi.github.io.git master
+
 cd -
 
 git add .
