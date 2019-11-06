27d0a6d528d60d17c43ead5987ed08cceb5dc1c0
rikumiyu <rikumiyu@tencent.com>
2019-11-06 10:44:33 +0800

    deploy

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,36 +1,34 @@
 module.exports = {
   locales: {
     '/': {
-      lang: 'zh', // For disqus compatibility
-    },
+      lang: 'zh' // For disqus compatibility
+    }
   },
   title: '[ RKM ]',
   description: 'アクアテラリウム',
-  head: [
-    ['link', { rel: 'icon', href: '/favicon.png', type: 'image/x-icon' }]
-  ],
+  head: [['link', { rel: 'icon', href: '/favicon.png', type: 'image/x-icon' }]],
   theme: 'journal',
   themeConfig: {
     nav: [
       {
         text: '关于',
-        link: '/about/',
+        link: '/about/'
       },
       {
         text: '朋友们',
-        link: '/friends/',
+        link: '/friends/'
       },
       {
         text: '作品',
-        link: '/works/',
-      },
+        link: '/works/'
+      }
     ],
     modifyBlogPluginOptions(opts) {
       return {
         ...opts,
         comment: {
           service: 'disqus',
-          shortname: 'rikumi',
+          shortname: 'rikumi'
         }
       };
     }
